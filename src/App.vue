f64267aa33bb3e6158a6505ba071550d193d74d4
riffuu1 <pj85gyl@eduvaud.ch>
2025-11-03 13:30:06 +0100

    refactor: exo2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import sockGreen from './assets/images/socks_green.jpeg'
+import sockBlue from './assets/images/socks_blue.jpeg'
+</script>
 
 <template>
   <div class="product-display">
+    <img :src="sockGreen" alt="Green Socks" />
+    <img :src="sockBlue" alt="Blue Socks" />
     <div class="product-container">
       <div class="product-info">
         <h1>Socks</h1>
