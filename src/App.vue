b8d64c8fa10b258281309fb01a50d92045f6d17e
riffuu1 <pj85gyl@eduvaud.ch>
2025-11-03 14:11:01 +0100

    fix:checkout resolution

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
 import sockGreen from './assets/images/socks_green.jpeg'
 import sockBlue from './assets/images/socks_blue.jpeg'
+const url = "https://sekai.one/piece/saga-8?reprise=true"
+const instock = true
 </script>
 
 <template>
   <div class="product-display">
-    <img :src="sockGreen" alt="Green Socks" />
-    <img :src="sockBlue" alt="Blue Socks" />
     <div class="product-container">
       <div class="product-info">
         <h1>Socks</h1>
+        <div class="product-image">
+          <img :src="sockGreen" alt="Green Socks" />
+        </div>
+          <a :href="url" target="_blank" rel="noopener">Va regarder One Piece</a>
+          <p v-if="instock">En stock</p>
+          <p v-else>En rupture de stock</p>
       </div>
     </div>
   </div>
