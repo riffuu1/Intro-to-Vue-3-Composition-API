3a352e1a50fe745fb5fab1e977a82d29a7ec912e
riffuu1 <pj85gyl@eduvaud.ch>
2025-11-10 10:47:26 +0100

    refactor:L7-end

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-import { ref } from 'vue'
+import {computed, ref} from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
 import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
-const brand = ref('Vue Mastery')
+const brand = ref('CPNV')
+const title = computed(()=>{
+  return brand.value + ' ' + product.value
+})
 
 const image = ref(socksGreenImage)
 const inStock = ref(false)
@@ -12,15 +15,21 @@ const inStock = ref(false)
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
-  { id: 2234, color: 'green', image: socksGreenImage },
-  { id: 2235, color: 'blue', image: socksBlueImage },
+  { id: 2234, color: 'green', image: socksGreenImage,quantity:10 },
+  { id: 2235, color: 'blue', image: socksBlueImage,quantity:0 },
 ])
 
 const cart = ref(0)
 
 const addToCart = () => cart.value += 1
 
-const updateImage = (variantImage) => image.value = variantImage
+const updateVariant = (index) => {
+  selectedVariant.value = index
+  image.value = variants.value[index].image
+  inStock.value = variants.value[index].quantity > 0
+}
+const selectedVariant = ref(0)
+
 
 </script>
   
@@ -33,18 +42,18 @@ const updateImage = (variantImage) => image.value = variantImage
         <img v-bind:src="image">
       </div>
       <div class="product-info">
-        <h1>{{ product }}</h1>
+        <h1>{{title}}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
         <div 
-          v-for="variant in variants" 
-          :key="variant.id"
-          @mouseover="updateImage(variant.image)"
+          v-for="(variants, index) in variants"
+          :key="variants.id"
+          @mouseover="updateVariant(index)"
           class="color-circle"
-          :style="{ backgroundColor: variant.color }"
+          :style="{ backgroundColor: variants.color }"
         >
         </div>
         <button
